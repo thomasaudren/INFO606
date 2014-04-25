fc4f39fb3294aae47d655e3aeba69e14cba91e61
Nicolas <zeydix@gmail.com>
2014-04-25 11:52:09 +0200

    Ajout minime stats

--- application/views/statsByEleve.php
+++ application/views/statsByEleve.php
@@ -12,19 +12,38 @@ $res = $eleveC->getExercicesByIdEleve($idEleve);
 $ret="";
 
 $i=0;
-//La graine se récupère aussi via $res[$i]['graine'];
 if(isset($res[0]['error']))
 {
 	$ret.="Aucun exercice disponible pour cet élève";
 	$i=1;
 }
-
-while ($i < sizeof($res))
+else
 {
-	$ret.="<div>Exercice : ".$res[$i]['libExo']."<br>Matière : ".$res[$i]['libMat']."<br>Score : ".$res[$i]['percent']."<br><hr>";
-	$i++;
+	$ret.='<table class="table table-bordered"><thead><tr><th>Nom de l\'exercice<th>Matière<th>Score</tr>';
+	while ($i < sizeof($res))
+	{
+		//La graine se récupère aussi via $res[$i]['graine'];
+		if($res[$i]['percent']>50)
+		{
+			$ret.="<tr class='success'><td>".$res[$i]['libExo']."<td>".$res[$i]['libMat']."<td>".$res[$i]['percent'];
+		}
+		else if($res[$i]['percent']<50)
+		{
+			$ret.="<tr class='danger'><td>".$res[$i]['libExo']."<td>".$res[$i]['libMat']."<td>".$res[$i]['percent'];
+		}
+		else if($res[$i]['percent']==50)
+		{
+			$ret.="<tr class='warning'><td>".$res[$i]['libExo']."<td>".$res[$i]['libMat']."<td>".$res[$i]['percent'];	
+		}
+		$i++;
+	}
+	$ret.='</table><br><hr>';
 }
+echo $ret;
 
-	echo $ret;
+
+
+
+
 
 ?>
